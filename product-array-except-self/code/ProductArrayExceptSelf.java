bd2526fe7db6582df9b80b73f014e8b779a45ef5
Victor Hugo Molinar <vhmolinar@gmail.com>
2022-12-11 22:50:18 -0300

    feat: product array except self; reverse-list

---
+++ product-array-except-self/code/ProductArrayExceptSelf.java
@@ -0,0 +1,39 @@
+package code;
+
+import java.util.Arrays;
+
+public class ProductArrayExceptSelf {
+
+  public static void main(String[] args) {
+    var test = new ProductArrayExceptSelf();
+    test.validate(new int [] { -1, 1, 0, -3, 3 }, new int [] { 0,0,9,0,0 });
+    test.validate(new int [] { 1,2,3,4 }, new int [] { 24,12,8,6 });
+  }
+
+  private void validate(int [] input, int[] expectedOutput) {
+    var output = productExceptSelf(input);
+    assert Arrays.equals(output, expectedOutput)
+      : String.format("Result should be %s, but got %s", Arrays.toString(expectedOutput), Arrays.toString(output));
+  }
+
+  private int[] productExceptSelf(int[] nums) {
+    var resultingProduct = new int [nums.length];
+
+    var historicProduct = new int [nums.length];
+    var accumulativeProduct = 1;
+
+    for (var i = 0; i < nums.length; i += 1) {
+      resultingProduct[i] = accumulativeProduct;
+      accumulativeProduct *= nums[i];
+      historicProduct[i] = accumulativeProduct;
+    }
+
+    accumulativeProduct = nums[nums.length - 1];
+    for (var i = nums.length - 2; i >= 0; i -= 1) {
+      resultingProduct[i] *= accumulativeProduct;
+      accumulativeProduct *= nums[i];
+    }
+
+    return resultingProduct;
+  }
+}
---
+++ reverse-list/README.md
@@ -0,0 +1,5 @@
+# Reverse list
+
+
+## Challenge Description:
+Write a function that takes a list and returns it in the reverse order
